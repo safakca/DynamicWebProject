631bb58d872482279a4cc49a85b9b24d25b00e60
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-01-25 15:40:02 +0300

    [DEV-UP] : ProductConfiguration

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -2,19 +2,20 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using EntityLayer.Concrete;
+using DataAccessLayer.Configurations;
 
 namespace DataAccessLayer.Concrete;
 
-public class Context : IdentityDbContext<AppUser,AppRole,int>
+public class Context : IdentityDbContext<AppUser, AppRole, int>
 {
-    public Context(DbContextOptions<Context> options): base(options) { }
+    public Context(DbContextOptions<Context> options) : base(options) { }
 
     public DbSet<Product> Products => this.Set<Product>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-      //  modelBuilder.ApplyConfiguration(new ProductConfiguration()); 
-      base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        base.OnModelCreating(modelBuilder);
     }
 }
 
---
+++ DataAccessLayer/Configurations/ProductConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DataAccessLayer.Configurations;
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable(nameof(Product));
+        builder.HasKey(x => x.Id);
+
+        Product[] products =
+        {
+            new() {Id = 1, Name="Computer", Price=32460, Stock=250},
+            new() {Id = 2, Name="Phone", Price=12500, Stock=550},
+            new() {Id = 3, Name="Keyboard", Price=2300, Stock=1000},
+            new() {Id = 4, Name="Mouse", Price=1200, Stock=1500},
+        };
+        builder.HasData(products);
+    }
+}
