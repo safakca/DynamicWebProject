a3cbb3500b558a86915a62dad666c37fe42f2497
Şafak AKÇA <40564836+safakca@users.noreply.github.com>
2023-03-20 16:48:04 +0300

    [DEV-UP] : isExist user

--- BusinessLayer/Features/CQRS/Handlers/Users/CheckUserRequestHandler.cs
+++ BusinessLayer/Features/CQRS/Handlers/Users/CheckUserRequestHandler.cs
@@ -2,16 +2,15 @@
 using BusinessLayer.Repositories;
 using DtoLayer.Concrete.Jwt;
 using EntityLayer.Concrete;
+using EntityLayer.Enums;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Identity; 
 
 namespace BusinessLayer.Features.CQRS.Handlers.Users;
 public class CheckUserRequestHandler : IRequestHandler<CheckUserQueryRequest, CheckUserResponseDto>
-{ 
+{
     private readonly UserManager<AppUser> _userManager;
-    private readonly RoleManager<AppRole> _roleManager;
+    private readonly RoleManager<AppRole> _roleManager; 
 
     public CheckUserRequestHandler(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager)
     {
@@ -21,22 +20,17 @@ public class CheckUserRequestHandler : IRequestHandler<CheckUserQueryRequest, Ch
 
     public async Task<CheckUserResponseDto> Handle(CheckUserQueryRequest request, CancellationToken cancellationToken)
     {
-        var dto = new CheckUserResponseDto();
-        //var user = await _userRepository.GetByFilterAsync(x => x.UserName == request.Username && x.Password == request.Password);
-        var user = await _userManager.Users.ToListAsync();
-
+        var dto = new CheckUserResponseDto();  
+        var user = await _userManager.FindByNameAsync(request.Username); 
+  
         if (user == null)
         {
             dto.IsExist = false;
         }
         else
         {
-
-            //dto.Username = request.Username;
-            //dto.Id = user.Id;
-            //dto.IsExist = true;
-            //var role = await _roleRepository.GetByFilterAsync(x => x.Id == user.AppRoleId);
-            //dto.Role = role?.Defination;
+            dto.Username = request.Username; 
+            dto.IsExist = true; 
         }
         return dto;
     }
