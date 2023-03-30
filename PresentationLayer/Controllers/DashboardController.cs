fb1638d94fccc4d6bb0a3ed0871775e69e945e31
safakca <safakkca@gmail.com>
2023-03-30 05:46:23 +0300

    [DEV-UP] : added weather from RapidApi in dashboard

--- PresentationLayer/Controllers/DashboardController.cs
+++ PresentationLayer/Controllers/DashboardController.cs
@@ -1,9 +1,12 @@
-﻿using BusinessLayer.Repositories;
+﻿using Azure;
+using BusinessLayer.Repositories;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using PresentationLayer.Models.Weather;
 
 namespace PresentationLayer.Controllers;
 public class DashboardController : Controller
@@ -19,24 +22,29 @@ public class DashboardController : Controller
     {
         List<Article> articleList = await _articleRepository.GetAllAsync();
         ViewBag.Article = articleList;
-
+ 
         var client = new HttpClient();
         var request = new HttpRequestMessage
         {
             Method = HttpMethod.Get,
-            RequestUri = new Uri("https://open-weather13.p.rapidapi.com/city/landon"),
+            RequestUri = new Uri("https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Istanbul&country=Turkey"),
             Headers =
             {
                 { "X-RapidAPI-Key", "b881181406msh32a0ead044431adp1d4417jsn2e90cd1f8a96" },
-                { "X-RapidAPI-Host", "open-weather13.p.rapidapi.com" },
+                { "X-RapidAPI-Host", "weather-by-api-ninjas.p.rapidapi.com" },
             },
         };
         using (var response = await client.SendAsync(request))
         {
             response.EnsureSuccessStatusCode();
             string body = await response.Content.ReadAsStringAsync();
-            JObject valuePairs = JObject.Parse(body);   
-            ViewBag.Weater = valuePairs;
+
+            // string inComingApiModel =  "{'cloud_pct': 'value', 'temp': 'value', 'feels_like': 'value', 'humidity': 'value', 'min_temp': 'value', 'max_temp': 'value', 'wind_speed': 'value'}";
+            // should be use deserialize to get
+            // should be use serialize to update and create
+            // body convert to ViewModel 
+            WeatherViewModel weathers = JsonConvert.DeserializeObject<WeatherViewModel>(body);
+            ViewBag.Weather = weathers;   
         } 
 
         return View();
@@ -44,3 +52,6 @@ public class DashboardController : Controller
      
 
 }
+
+
+ 
--- PresentationLayer/Models/Weather/WeatherViewModel.cs
+++ PresentationLayer/Models/Weather/WeatherViewModel.cs
@@ -1,4 +1,11 @@
 ﻿namespace PresentationLayer.Models.Weather;
 public class WeatherViewModel
 {
+    public int temp { get; set; }
+    public int min_temp { get; set; }
+    public int max_temp { get; set; }
+    public int cloud_pct { get; set; }
+    public float wind_speed { get; set; }
+    public int humidity { get; set; }
 }
+
